ba8c1b47e05c4716fd06c84ab286bb3efaec47cc
GaiaAndresen <gaia.ulv@gmail.com>
2022-08-31 20:05:12 +0200

    Tests passed including own empty test.

--- src/fasta-recs.py
+++ src/fasta-recs.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 def main():
     argparser = argparse.ArgumentParser(
         description="Extract Simple-FASTA records"
@@ -11,7 +10,26 @@ def main():
     )
     args = argparser.parse_args()
 
-    print(f"Now I need to process the records in {args.fasta}")
+    # print(f"Now I need to process the records in {args.fasta}")
+
+
+    #Assume resonable file
+
+    for e in args.fasta.read().split(">")[1:]:
+        name, seq = e.split("\n", 1)
+        print(name.strip(), seq.replace("\n", "").strip(), sep='\t')
+
+
+    #Alternative version
+    # seq = ""
+    # for e in args.fasta:
+    #     if e[0]== '>':
+    #         if len(seq)>0:
+    #             print(seq)
+    #         seq = f'{e[1:].strip()}\t'
+    #     else: 
+    #         seq = seq + e.strip()
+    # print(seq)
 
 
 if __name__ == '__main__':
--- src/get-subseqs.py
+++ src/get-subseqs.py
@@ -18,8 +18,17 @@ def main():
     )
     args = argparser.parse_args()
 
-    print(f"Now I need to process the records in {args.fasta}")
-    print(f"and the coordinates in {args.coords}")
+    # print(f"Now I need to process the records in {args.fasta}")
+    # print(f"and the coordinates in {args.coords}")
+
+    seqs = {}
+    for e in args.fasta.read().split(">")[1:]:
+        name, seq = e.split("\n", 1)
+        seqs[name.strip()] = seq.replace("\n", "").strip()
+    
+    for coor in args.coords:
+        name, coor1, coor2 = coor.split()
+        print(seqs[name.strip()][int(coor1)-1:int(coor2)-1])
 
 
 if __name__ == '__main__':
